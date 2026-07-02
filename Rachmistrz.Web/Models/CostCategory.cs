a65b1dbeecbe3fea53ef36dff33761a1cb5d02d3
mhanusiak <maciej.hanusiak7@gmail.com>
2026-07-02 21:01:43 +0200

    feat: add supplier and cost category models

---
+++ Rachmistrz.Web/Models/CostCategory.cs
@@ -0,0 +1,13 @@
+﻿namespace Rachmistrz.Web.Models;
+
+public class CostCategory
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = string.Empty;
+
+    public string Description { get; set; } = string.Empty;
+
+    // Can not delete, only activate/deactivate
+    public bool IsActive { get; set; } = true;
+}
