664ba1b73ff9d3e9dd7c9a719960039241aa3afb
mhanusiak <maciej.hanusiak7@gmail.com>
2026-07-06 17:33:25 +0200

    feat: seed application roles

---
+++ Rachmistrz.Web/Seed/DatabaseSeeder.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Rachmistrz.Web.Constants;
+
+
+namespace Rachmistrz.Web.Seed
+{
+    public class DatabaseSeeder
+    {
+        public static async Task SeedRolesAsync(IServiceProvider serviceProvider)
+        {
+            using var scope = serviceProvider.CreateScope();
+
+            var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+
+            string[] roles =
+                [
+                    RoleNames.Admin,
+                    RoleNames.Accounting,
+                    RoleNames.BranchManager,
+                    RoleNames.Employee
+                ];
+
+            foreach (var role in roles)
+            {
+                var roleExists = await roleManager.RoleExistsAsync(role);
+
+                if (!roleExists)
+                {
+                    await roleManager.CreateAsync(new IdentityRole(role));
+                }
+            }
+
+        }
+    }
+}
