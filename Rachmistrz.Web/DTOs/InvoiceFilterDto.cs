b8526d89cbd198f14c29cc6934fc59f1d1366577
mhanusiak <maciej.hanusiak7@gmail.com>
2026-08-07 23:03:35 +0200

    feat: apply invoice filters in service

--- Rachmistrz.Web/DTOs/InvoiceFilterDto.cs
+++ Rachmistrz.Web/DTOs/InvoiceFilterDto.cs
@@ -8,7 +8,7 @@ namespace Rachmistrz.Web.DTOs
         public InvoiceStatus? Status { get; set; }
         public int? SupplierId { get; set; }
         public int? BranchId { get; set; }
-        public DateTime? DueDateForm { get; set; }
+        public DateTime? DueDateFrom { get; set; }
         public DateTime? DueDateTo { get; set; }
     }
 }
